737969c855ad00bf508d5f085a89313f076992f5
hypez33 <64811560+hypez33@users.noreply.github.com>
2025-08-19 16:11:20 +0200

    Add files via upload

---
+++ .bat
@@ -0,0 +1,7 @@
+echo "# azkiener" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/hypez33/azkiener.git
+git push -u origin main
---
+++ README.md
@@ -0,0 +1,3 @@
+"# azkiener" 
+"# azkiener" 
+"# azkiener" 
