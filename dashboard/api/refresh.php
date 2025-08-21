bed634c687ab5c54911ba03ef677b0105c107680
hypez33 <64811560+hypez33@users.noreply.github.com>
2025-08-21 18:29:58 +0200

    Delete dashboard/api/refresh.php

--- dashboard/api/refresh.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-header('Content-Type: application/json; charset=utf-8');
-header('Cache-Control: no-store');
-$_GET['force'] = '1';
-require __DIR__ . '/vehicles.php';
